8a5e0a47a804a25b752ae5bc6fbdf4a2848791d8
Anders Busch <andersbusch@gmail.com>
2019-10-18 19:22:06 +0200

    needed pthreads

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "libdill.h"
 
-int main(void)
-{
+
+coroutine void worker(const char *text) {
+  while(1) {
+    printf("%s\n", text);
+    msleep(now() + random() % 500);
+  }
+}
+
+int main(void) {
+    go(worker("Hello!"));
+    go(worker("World!"));
+
+    msleep(now() + 5000);
+
     printf("Hello world\n");
     return 0;
 }
